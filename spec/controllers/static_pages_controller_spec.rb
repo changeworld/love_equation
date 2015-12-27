f0d2dc035ef1b57785f0ce257a173821222c554f
changeworld <changesworlds@gmail.com>
2015-12-27 21:08:56 +0900

    Add static pages

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+
+  describe "GET home" do
+    it "returns http success" do
+      get :home
+      expect(response).to be_success
+    end
+  end
+
+  describe "GET help" do
+    it "returns http success" do
+      get :help
+      expect(response).to be_success
+    end
+  end
+end
