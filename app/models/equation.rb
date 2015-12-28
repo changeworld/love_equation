6472e9f2233cd203302e54115e99d493eea29373
changeworld <changesworlds@gmail.com>
2015-12-29 00:50:45 +0900

    Refactoring

--- app/models/equation.rb
+++ app/models/equation.rb
@@ -5,10 +5,9 @@ class Equation
     # Love equation first
     # F(0) = 0.5
     # F(n) = F(n-1) * F(n-1) + (1+F(n-1))/2 * (1-F(n-1))
-    f0 = 0.5
-    i.times do
-      f0 = f0 * f0 + (1 + f0) / 2 * (1 - f0)
-    end
-    f0
+    #      = F(n-1) * F(n-1) + (F(n-1) * F(n-1) + 1)/2
+    #      = (F(n-1) * F(n-1) + 1)/2
+    @arr_equation_1st ||= []
+    @arr_equation_1st[i] ||= (i < 1) ? 0.5 : (equation_1st(i - 1)**2 + 1) / 2
   end
 end
