c2f5225aa7f89fc871468930d53e4760bdc18df1
changeworld <changesworlds@gmail.com>
2015-12-29 00:57:59 +0900

    Modify Equation
    - get_equation_1st

--- app/models/equation.rb
+++ app/models/equation.rb
@@ -10,4 +10,9 @@ class Equation
     @arr_equation_1st ||= []
     @arr_equation_1st[i] ||= (i < 1) ? 0.5 : (equation_1st(i - 1)**2 + 1) / 2
   end
+
+  def get_equation_1st(i)
+    equation_1st(i)
+    @arr_equation_1st
+  end
 end
