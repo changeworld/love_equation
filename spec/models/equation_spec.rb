fdc7cb8863087a4b8724bf539f5846c61a83b2d0
changeworld <changesworlds@gmail.com>
2015-12-30 16:36:09 +0900

    Modify Equation
    - get_love_equation_1st_score

--- spec/models/equation_spec.rb
+++ spec/models/equation_spec.rb
@@ -42,4 +42,18 @@ RSpec.describe Equation, type: :model do
       expect(@equation.love_times(20, 1)).to eq(7)
     end
   end
+
+  describe 'get_love_equation_1st_score' do
+    before(:all) do
+      @equation = Equation.new
+    end
+
+    it '今まで16回の恋愛をした34歳の期待スコア is 51' do
+      expect(@equation.get_love_equation_1st_score(34, 16)).to eq(51)
+    end
+
+    it '今まで1回の恋愛をした20歳の期待スコア is 83' do
+      expect(@equation.get_love_equation_1st_score(20, 1)).to eq(83)
+    end
+  end
 end
