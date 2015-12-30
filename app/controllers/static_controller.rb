0abaab3a7fa243f8ba9e8772616023697959ea28
changeworld <changesworlds@gmail.com>
2015-12-30 16:39:46 +0900

    Modify Static
    - jadge

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,6 +4,14 @@ class StaticController < ApplicationController
     @track_record_items = (1..100).map { |n| [n, n] }
   end
 
+  def jadge
+    age = params[:age].to_i
+    track_record = params[:track_record].to_i
+    equation = Equation.new
+    @count = equation.love_times(age, track_record)
+    @score = equation.get_love_equation_1st_score(age, track_record)
+  end
+
   def help
   end
 end
