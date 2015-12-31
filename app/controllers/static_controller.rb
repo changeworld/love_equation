1b93d7d8290f8f674f4e44bd24f303ad2b42b9b0
changeworld <changesworlds@gmail.com>
2015-12-31 10:27:48 +0900

    Modify jadge logic

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
   def home
-    @age_items = (18..35).map { |n| [n, n] }
+    @age_items = (18..100).map { |n| [n, n] }
     @track_record_items = (1..100).map { |n| [n, n] }
   end
 
@@ -8,8 +8,7 @@ class StaticController < ApplicationController
     age = params[:age].to_i
     track_record = params[:track_record].to_i
     equation = Equation.new
-    @count = equation.love_times(age, track_record)
-    @score = equation.get_love_equation_1st_score(age, track_record)
+    @judgment_result = equation.get_judgment_result(age, track_record)
   end
 
   def help
