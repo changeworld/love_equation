8c2d02ef69b07bca374cba01be4e7244356c6159
changeworld <changesworlds@gmail.com>
2016-01-09 15:02:30 +0900

    Add RSpec
    - View static#home

--- spec/views/static/home.html.slim_spec.rb
+++ spec/views/static/home.html.slim_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "static/home.html.slim", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'static/home.html.slim', type: :view do
+  it 'displays home page' do
+    age_items = (18..100).map { |n| [n, n] }
+    times = (1..100).map { |n| [n, n] }
+    assign(:age_items, age_items)
+    assign(:times, times)
+    render
+    expect(rendered).to render_template(:home)
+  end
 end
