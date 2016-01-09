040aefc33652c152155b7dac962b289982dae861
changeworld <changesworlds@gmail.com>
2016-01-09 15:12:34 +0900

    Add RSpec
    - View static#jadge

--- spec/views/static/jadge.html.slim_spec.rb
+++ spec/views/static/jadge.html.slim_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "static/jadge.html.slim", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'static/jadge.html.slim', type: :view do
+  it 'displays jadge page' do
+    judgment_result = 'ぴよぴよ'
+    assign(:judgment_result, judgment_result)
+    render
+    expect(rendered).to render_template(:jadge)
+    expect(rendered).to include(judgment_result)
+  end
 end
