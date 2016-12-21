eaa98823c8fc7a207b7356346c2b7def5911e3f1
Takashi Takebayashi <changesworlds@gmail.com>
2016-12-21 14:46:03 +0900

    Fix pending
    - Not yet implemented

--- spec/helpers/static_helper_spec.rb
+++ spec/helpers/static_helper_spec.rb
@@ -11,5 +11,7 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe StaticHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'equivalence' do
+    expect(true).to eq(true)
+  end
 end
