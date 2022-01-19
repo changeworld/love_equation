7aa02d7a421649fe3eac7bbb4f3543155b68fd59
Takashi Takebayashi <changesworlds@gmail.com>
2022-01-20 08:01:32 +0900

    Upgrade Rails 5.2.6 -> 6.0.0

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'static#index'
   post '/achievement' => 'static#achievement'
   post '/prospect' => 'static#prospect'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
