0c9cc01877a1fa83cdc3dcabd19eec7d29dfdf9a
Takashi Takebayashi <changesworlds@gmail.com>
2022-01-31 12:47:03 +0900

    Modify routes
    - Add get achievement
    - Add get prospect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'static#index'
+  get  '/achievement' => 'static#index'
   post '/achievement' => 'static#achievement'
-  post '/prospect' => 'static#prospect'
+  get  '/prospect'    => 'static#index'
+  post '/prospect'    => 'static#prospect'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
