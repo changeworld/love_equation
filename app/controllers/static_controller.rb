a6350d59add272da5d1a373345d8228d033b4906
Takashi Takebayashi <changesworlds@gmail.com>
2022-01-31 12:49:03 +0900

    Add render action
    - index

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,6 +3,7 @@ class StaticController < ApplicationController
     @age_items = (18..100).map { |n| [n, n] }
     @achievement_times = (1..100).map { |n| [n, n] }
     @prospect_times = (0..100).map { |n| [n, n] }
+    render action: :index
   end
 
   def achievement
