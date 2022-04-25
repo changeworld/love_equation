2cfa215e4c66ca1edc5c695817398c7bd86cd96f
Takashi Takebayashi <changesworlds@gmail.com>
2022-04-25 13:32:31 +0900

    Upgrade Rails 7.0.0 -> 7.0.1

--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,8 @@ Bundler.require(*Rails.groups)
 module LoveEquation
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
     # Default locale Japanese
     config.i18n.default_locale = :ja
-    Rails.application.config.action_controller.urlsafe_csrf_tokens = false
-    config.active_record.legacy_connection_handling = false
 
     # Configuration for the application, engines, and railties goes here.
     #
