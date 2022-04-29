fb1d6ee27b3cef7a2c760fe50b35415621ee923d
Takashi Takebayashi <changesworlds@gmail.com>
2022-04-29 11:13:19 +0900

    Tweak

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ Bundler.require(*Rails.groups)
 module LoveEquation
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
     # Default locale Japanese
     config.i18n.default_locale = :ja
 
