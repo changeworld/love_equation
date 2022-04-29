7e9e870a79a730a0ee8d20f48a5a6215edb621fc
Takashi Takebayashi <changesworlds@gmail.com>
2022-04-29 11:13:02 +0900

    R.I.P. PostgreSQL

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,18 @@
 require_relative "boot"
 
-require "rails/all"
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+# require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,7 +23,6 @@ module LoveEquation
     # Initialize configuration defaults for originally generated Rails version.
     # Default locale Japanese
     config.i18n.default_locale = :ja
-    config.active_record.legacy_connection_handling = false
 
     # Configuration for the application, engines, and railties goes here.
     #
