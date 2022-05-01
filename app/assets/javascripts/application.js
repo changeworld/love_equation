7666cff100d12d11b696134dc63be5af72304907
Takashi Takebayashi <changesworlds@gmail.com>
2022-05-01 11:28:48 +0900

    Turbo in Turbolinks out

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets-custom
