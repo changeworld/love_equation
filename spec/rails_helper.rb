3aec1474a0a005ddd24dedb41d4d8adb16bdbae9
Takashi Takebayashi <changesworlds@gmail.com>
2022-06-21 10:16:54 +0900

    Fix Style/FetchEnvVar
    -  Use ENV.fetch('RAILS_ENV') or ENV.fetch('RAILS_ENV', nil) instead of ENV['RAILS_ENV']

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV['RAILS_ENV'] ||= 'test'
+ENV.fetch('RAILS_ENV', 'test')
 require 'spec_helper'
 require File.expand_path('../config/environment', __dir__)
 require 'rspec/rails'
